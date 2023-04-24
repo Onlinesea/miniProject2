8370353a0b82eec0a41356200e6c81d6e8e6398b
Onlinesea <69972150+Onlinesea@users.noreply.github.com>
2023-04-24 21:41:05 +0800

    update

--- server/src/main/java/vttp/miniproject2/server/FirebaseConfig.java
+++ server/src/main/java/vttp/miniproject2/server/FirebaseConfig.java
@@ -1,6 +1,9 @@
 package vttp.miniproject2.server;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.StringReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -13,16 +16,27 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
 
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonReader;
+
 @Configuration
 public class FirebaseConfig {
     
     // @Value("${fire.service.account}")
     // private String config;
+    
+    @Value("${FIREBASE_SERVICE_ACCOUNT}")
+    private String config;
+
 
     @Bean
     public FirebaseMessaging firebaseMessaging() throws IOException {
-    GoogleCredentials googleCredentials = GoogleCredentials
-            .fromStream(new ClassPathResource("firebase-service-account.json").getInputStream());
+            GoogleCredentials googleCredentials = GoogleCredentials.fromStream(
+                    new ByteArrayInputStream(config.getBytes(StandardCharsets.UTF_8))
+            );
+    // GoogleCredentials googleCredentials = GoogleCredentials
+    //         .fromStream(new ClassPathResource("firebase-service-account.json").getInputStream());
     FirebaseOptions firebaseOptions = FirebaseOptions
             .builder()
             .setCredentials(googleCredentials)
